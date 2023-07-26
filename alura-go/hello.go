0b7acf31ef17eadd0e8c176d140dabe296dc75e7
Rafael Monteiro <rafael.monteiro.porto@outlook.com>
2023-07-26 07:39:34 -0300

    Initial commit

---
+++ alura-go/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nome := "Rafael"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("3 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variavel comando é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+}
